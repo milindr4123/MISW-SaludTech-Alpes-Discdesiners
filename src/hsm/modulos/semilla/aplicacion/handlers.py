b073821cd2698b39847c10088b24f27ad57e1d09
miguel parra <ma.parrat@uniandes.edu.co>
2025-02-23 18:58:57 -0500

    create seed

--- src/hsm/modulos/semilla/aplicacion/handlers.py
+++ src/hsm/modulos/semilla/aplicacion/handlers.py
@@ -1,13 +1,13 @@
-# from aeroalpes.modulos.vuelos.dominio.eventos import ReservaCreada, ReservaCancelada, ReservaAprobada, ReservaPagada
-# from aeroalpes.seedwork.aplicacion.handlers import Handler
-# from aeroalpes.modulos.vuelos.infraestructura.despachadores import Despachador
+from hsm.modulos.semilla.dominio.eventos import SemillaCreada
+from hsm.seedwork.aplicacion.handlers import Handler
+from hsm.modulos.semilla.infraestructura.despachadores import Despachador
 
-# class HandlerReservaIntegracion(Handler):
+class HandlerSemillaIntegracion(Handler):
 
-#     @staticmethod
-#     def handle_reserva_creada(evento):
-#         despachador = Despachador()
-#         despachador.publicar_evento(evento, 'eventos-reserva')
+     @staticmethod
+     def handle_semilla_creada(evento):
+         despachador = Despachador()
+         despachador.publicar_evento(evento, 'eventos-semilla')
 
 #     @staticmethod
 #     def handle_reserva_cancelada(evento):
